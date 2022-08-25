c3ed781f39d2bd2578dbf093a022e139ba9b43de
Student7 <student7@school-7711.local>
2022-08-25 09:26:15 +0300

    Added Scenario 1 - Login with locked_out_user

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -6,7 +6,13 @@ describe("Swag Labs", () => {
     LoginPage.visit();
   });
 
-  it("Scenario 1", () => {
-    // TODO: implement me
+  it("Scenario 1 - Login with locked_out_user", () => {
+    // - Enter username - “locked_out_user”
+    // - Enter password
+    // - Validate that user sees error - “Epic sadface: Sorry, this user has been locked out.”
+    LoginPage.usernameInput.type("locked_out_user")
+    LoginPage.passwordInput.type("secret_sauce")
+    LoginPage.loginBtn.click()
+    LoginPage.errorMsg.should("have.text", "Epic sadface: Sorry, this user has been locked out.")
   });
 });
--- cypress/pageObjects/Login.page.js
+++ cypress/pageObjects/Login.page.js
@@ -5,8 +5,17 @@ class LoginPage extends BasePage {
     return "/";
   }
 
-  static get xxx() {
-    return cy.get("x[y=z]");
+  static get usernameInput() {
+    return cy.get('[data-test="username"]')
+  }
+  static get passwordInput(){
+    return cy.get('[data-test="password"]')
+  }
+  static get loginBtn(){
+    return cy.get('[data-test="login-button"]')
+  }
+  static get errorMsg(){
+    return cy.get('[data-test="error"]')
   }
 }
 
